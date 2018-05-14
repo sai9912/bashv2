150da77897731b7d40186b9bdd615d41abe0bd36
sai9912 <root@server.corp.ncodeit.com>
2018-05-14 16:48:52 +0530

    arithamatic

---
+++ arithamatic.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+a=123
+b=12
+
+c=`expr $a + $b` # addition
+echo $c
+
+c=`expr $a \* $b` # multiplication
+echo $c
+
+c=`expr $a / $b` # division
+echo $c
+
+c=`expr $a % $b` # residual
+echo $c
+
+
